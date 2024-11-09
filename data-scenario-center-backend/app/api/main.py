989cbedb2de21865ac81091f80c3a9cfd9c97e4e
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2024-11-09 19:05:11 +0900

    commit

--- data-scenario-center-backend/app/api/main.py
+++ data-scenario-center-backend/app/api/main.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 
 from app.api.routes import DataScenarioRouter
-from app.api.routes import RunningDataScenarioRouter
 
 api_router = APIRouter()
 api_router.include_router(DataScenarioRouter.router)
-api_router.include_router(RunningDataScenarioRouter.router)
