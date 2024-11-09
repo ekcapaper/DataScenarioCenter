bc35806217791bd49901e69d02a1e8153680ee74
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2024-11-09 19:03:53 +0900

    commit

--- data-scenario-center-backend/app/api/routes/DataScenarioRouter.py
+++ data-scenario-center-backend/app/api/routes/DataScenarioRouter.py
@@ -1,3 +1,5 @@
+from tkinter.font import names
+
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 from app.api.dto.CamelModel import CamelModel
@@ -12,6 +14,7 @@ def get_data_scenario_manager(data_scenario_center_settings: DataScenarioCenterS
 
 
 # 시나리오 목록 조회
+# 추가된 데이터의 반영 필요
 class DataScenarioDto(CamelModel):
     name: str
     description: str
@@ -25,7 +28,7 @@ class DataScenarioListDto(CamelModel):
 
 
 @router.get("/scenarios", response_model=ResponseDSC[DataScenarioListDto])
-def get_data_scenarios(
+async def get_data_scenarios(
     data_scenario_manager: DataScenarioManager = Depends(get_data_scenario_manager),
 ):
     data_scenario_dto_list = list(map(
@@ -43,3 +46,41 @@ def get_data_scenarios(
         },
         "error": None
     }
+
+@router.get("/scenarios/{scenario-name}", response_model=ResponseDSC[DataScenarioDto])
+async def get_data_scenario(
+    scenario_name: str,
+    data_scenario_manager: DataScenarioManager = Depends(get_data_scenario_manager),
+):
+    data_scenario = data_scenario_manager.get_data_scenario(scenario_name).data_scenario
+    return {
+        "success": True,
+        "data": DataScenarioDto(
+            name=data_scenario.name,
+            description=data_scenario.description,
+            conda_environment=data_scenario.conda_environment,
+            script_path=data_scenario.script_path_str
+        ),
+        "error": None
+    }
+
+
+@router.post("/scenarios/{scenario-name}/start")
+async def start_scenario(
+    scenario_name: str,
+    data_scenario_manager: DataScenarioManager = Depends(get_data_scenario_manager),
+):
+    data_scenario_manager.start_data_scenario(scenario_name)
+    return ResponseDSC(success=True)
+
+
+# 시나리오 정지
+@router.post("/scenarios/{scenario-name}/stop")
+async def stop_scenario(
+    scenario_name: str,
+    data_scenario_manager: DataScenarioManager = Depends(get_data_scenario_manager),
+):
+    await data_scenario_manager.stop_data_scenario(scenario_name)
+    return ResponseDSC(
+        success=True,
+    )
